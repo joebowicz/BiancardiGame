452bc497a7d3c69ab7bb4e34e4df77380b902687
joebowicz <joebowicz@gmail.com>
2017-05-16 13:02:18 -0500

    Update Self.java

--- BiancardiGame/src/Self.java
+++ BiancardiGame/src/Self.java
@@ -2,7 +2,29 @@
  * Joe Gavin Charlie
  * 
  */
-public class Self extends Contestant
-{
 
+public class Self extends Contestant{
+
+	private int points;
+	private String name;
+	private int height;
+	private int weight;
+	private int IQ;
+	private double GPA;
+	
+	public Self(int p,String name, int h,int w, int iq, double gpa)
+	{
+		super(p, getName(), h, w, iq, gpa);
+	}
+	
+	public void answer()
+	{
+		
+	}
+	
+	public static String getName()
+	{
+		return "Joe";
+	}
+	
 }
